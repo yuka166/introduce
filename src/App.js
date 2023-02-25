b7c3354499474343a212c857b41edaee009bc954
yuka166@github <hoangdat1606@gmail.com>
2023-02-26 00:21:31 +0700

    Add new content, styling responsive

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleDown, faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
 import ScrollReveal from 'scrollreveal'
 import Skill from './components/ui/skill/skill';
 import SkillArr from './data/skilldata'
@@ -57,6 +58,20 @@ function App() {
     swipeToSlide: true,
     nextArrow: <TimelineArrowNext />,
     prevArrow: <TimelineArrowPrev />,
+    responsive: [
+      {
+        breakpoint: 820,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   }
 
   function ScrollDown() {
@@ -82,16 +97,21 @@ function App() {
   }, [count, text]);
 
   useEffect(() => {
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
     ScrollReveal().reveal('.skill-list>div', slideUp);
+    ScrollReveal().reveal('.primary-desc>div', { ...slideUp, delay: txt.length * 100 });
   }, []);
 
   return (
     <div className="container">
       <div className="first-word">
-        <div className="title">{text}</div>
+        <h1 className="title">{text}</h1>
         <div className="primary-desc">
           <div>A Front-End Developer whos loves to create beautiful, creative website.</div>
-          <div>Aiming to be an Full-Stack Developer in the future.</div>
+          <div>Aiming to be a Full-Stack Developer in the future.</div>
         </div>
         <div className="godown-btn"><button type='button' onClick={ScrollDown}><FontAwesomeIcon icon={faAngleDoubleDown} /></button></div>
       </div>
@@ -127,6 +147,17 @@ function App() {
       <div className="contact">
         <div className='title'>Contact me</div>
         <div className='title-border'></div>
+        <div className='social'>
+          <div className='social-text'>Email: hoangdat1606@gmail.com</div>
+          <div className='social-icon'>
+            <div>
+              <a href='https://www.facebook.com/izumi326/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faFacebook} /></a>
+            </div>
+            <div>
+              <a href='https://github.com/yuka166/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} /></a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
