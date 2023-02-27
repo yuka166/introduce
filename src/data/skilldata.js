cb5aa54a6de3e6608547300ce51c2aa33e2323fa
yuka166@github <hoangdat1606@gmail.com>
2023-02-27 17:53:29 +0700

    Edit skill field content

--- src/data/skilldata.js
+++ src/data/skilldata.js
@@ -1,9 +1,8 @@
 const skillArr = [
-    { id: 1, class: 'html', name: 'HTML', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt hic accusantium ratione necessitatibus beatae quas esse odit eaque officiis reprehenderit error, eveniet, molestiae, incidunt laudantium quod maiores vero sed! Saepe.' },
-    { id: 2, class: 'css', name: 'CSS', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt hic accusantium ratione necessitatibus beatae quas esse odit eaque officiis reprehenderit error, eveniet, molestiae, incidunt laudantium quod maiores vero sed! Saepe.' },
-    { id: 3, class: 'js', name: 'Javascript', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt hic accusantium ratione necessitatibus beatae quas esse odit eaque officiis reprehenderit error, eveniet, molestiae, incidunt laudantium quod maiores vero sed! Saepe.' },
-    { id: 4, class: 'reactjs', name: 'ReactJS', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt hic accusantium ratione necessitatibus beatae quas esse odit eaque officiis reprehenderit error, eveniet, molestiae, incidunt laudantium quod maiores vero sed! Saepe.' },
-    { id: 5, class: 'cs', name: 'C#', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt hic accusantium ratione necessitatibus beatae quas esse odit eaque officiis reprehenderit error, eveniet, molestiae, incidunt laudantium quod maiores vero sed! Saepe.' },
+    { id: 1, class: 'html', name: 'HTML', desc: 'Know most commonly used tags, not so good at class name, currently learning about SEO.' },
+    { id: 2, class: 'css', name: 'CSS', desc: 'Can make simple animation, know about box model, pretty bad at UX/UI but I will learn and improve it.' },
+    { id: 3, class: 'js', name: 'Javascript', desc: 'Beginner, still learning, know basic algorithm, still need to find solution on google sometimes.' },
+    { id: 4, class: 'reactjs', name: 'ReactJS', desc: 'Beginner, learning with Javascript at the same time.' },
 ]
 
 export default skillArr;
